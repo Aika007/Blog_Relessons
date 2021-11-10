d29e705d4ff00e26b85ebfbaf03b40e1b1badd00
Aika007 <jadyrajasha@gmail.com>
2021-11-10 21:43:16 +0600

    fixed bug

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# class Post(models.Model):
+#     title = models.CharField(max_length=150)
+#     create_at=models.DateTimeField(auto_now_add=True)
+#     author=models.CharField(50)
+#     discription=models.TextField()
+#     # image=models.ImageField()
+
--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path("hello/", hello_world, name='index'),
-    path("about/", about, name=about),
+    path("about/", about, name='about'),
     path("contact/", contact, name='contact'),
     path("post/", post, name='post') ,
 ]
